503e181007f10b6d3e8912330b8a12feba21ce23
René Løwe Jacobsen <rlj@rloewe.net>
2026-06-25 17:08:33 +0200

    Add CORS headers

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ export async function startServer(vestfor: Vestfor, port: number): Promise<void>
   const app: Express = express()
 
   app.use(json())
+  app.use((_req, res, next) => {
+    res.set('Cache-Control', 'no-store')
+    res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Methods', '*')
+    next()
+  })
 
   app.get('/', (_, res) => {
     res.status(200).json(vestfor.getData())
